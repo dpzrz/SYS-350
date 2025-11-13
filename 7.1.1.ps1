9f2ca0ce80d107d10756ddda800c3591e7572030
Diego Perez <112894794+dpzrz@users.noreply.github.com>
2025-11-13 18:29:23 -0500

    Create linked clone VM with differencing disk

--- 7.1.1.ps1
+++ 7.1.1.ps1
@@ -1,7 +1,24 @@
-New-VHD -Path "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\Win10-Linked.vhdx" -ParentPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\Win10-Linked.vhdx" -Differencing
 
-New-VM -Name "Win10-LinkedClone" -Generation 2 -MemoryStartupBytes 5GB -SwitchName "HyperV-WAN" -VHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\Win10-Linked.vhdx"
+$ParentVHD = "D:\VMs\BaseImages\Win10-Base.vhdx"
 
-Start-VM -Name "Win10-LinkedClone"
+$ChildVHD = "D:\VMs\LinkedClones\Win10-Linked.vhdx"
 
+$VMName = "Win10-LinkedClone"
+
+$VMSwitch = "Default Switch"
+
+
+Write-Host "Creating differencing disk (linked clone)..." -ForegroundColor Cyan
+New-VHD -Path $ChildVHD -ParentPath $ParentVHD -Differencing -SizeBytes 60GB
+
+Write-Host "Creating new VM using linked clone..." -ForegroundColor Cyan
+New-VM -Name $VMName -MemoryStartupBytes 4GB -Generation 2 -VHDPath $ChildVHD -SwitchName $VMSwitch
+
+Set-VMProcessor -VMName $VMName -Count 4
+Set-VM -Name $VMName -AutomaticCheckpointsEnabled $false
+
+Write-Host "Starting VM..." -ForegroundColor Cyan
+Start-VM -Name $VMName
+
+Write-Host "`nLinked clone successfully created!" -ForegroundColor Green
 
